e8ffdef6a9823e7505155d4a1e909beb3fced6e9
Alexander Kerner <lunohod@openinkpot.org>
2009-07-13 21:30:57 +0200

    ui redesign

--- src/usbwatcher.c
+++ src/usbwatcher.c
@@ -41,13 +41,6 @@
 #define USB_PLUGGED "USB-plugged"
 #define USB_UNPLUGGGED "USB-unplugged"
 
-typedef struct {
-	char *name;
-	void (*handler)();
-} usb_action_t;
-
-static usb_action_t *usb_actions = NULL;
-
 static Ecore_Evas *main_win;
 
 void x_shutdown(void* param) { 
@@ -66,6 +59,19 @@ static void die(const char* fmt, ...)
 	vfprintf(stderr, fmt, ap);
 	va_end(ap);
 	exit(EXIT_FAILURE);
+}
+
+static void
+key_handler(void *data, Evas *evas, Evas_Object *obj, void *event_info)
+{
+	Evas_Event_Key_Up* ev = (Evas_Event_Key_Up*)event_info;
+
+	if(!strcmp(ev->keyname, "1"))
+		load_massstorage();
+	else if(!strcmp(ev->keyname, "Escape") || !strcmp(ev->keyname, "2"))
+		do_nothing();
+	else if(!strcmp(ev->keyname, "XF86Search") || !strcmp(ev->keyname, "x"))
+		load_usbnet();
 }
 
 typedef struct
@@ -140,60 +146,14 @@ void do_nothing()
 	ecore_evas_hide(main_win);
 }
 
-static void draw_handler(Evas_Object* choicebox, Evas_Object* item,
-		int item_num, int page_position, void* param)
-{
-	usb_action_t *a = param;
-
-	char* buf;
-	buf = strdup(a[item_num].name);
-	edje_object_part_text_set(item, "text", buf);
-	free(buf);
-}
-
-static void page_handler(Evas_Object* choicebox, int cur_page, int total_pages,
-		void* param)
-{
-	Evas* canvas = evas_object_evas_get(choicebox);
-	Evas_Object* main_edje = evas_object_name_find(canvas, "main_edje");
-
-	choicebox_aux_edje_footer_handler(main_edje, "footer", cur_page, total_pages);
-}
-
-static void item_handler(Evas_Object* choicebox, int item_num, bool is_alt,
-		void* param)
-{
-	usb_action_t *a = param;
-
-	if(a[item_num].handler)
-		a[item_num].handler();
-}
-
 static void main_win_resize_handler(Ecore_Evas* main_win)
 {
 	Evas* canvas = ecore_evas_get(main_win);
 	int w, h;
 	evas_output_size_get(canvas, &w, &h);
 
-	Evas_Object* main_edje = evas_object_name_find(canvas, "main_edje");
-	evas_object_resize(main_edje, w, h);
-}
-
-static void key_up(void* param, Evas* e, Evas_Object* o, void* event_info)
-{
-	Evas_Event_Key_Up* ev = (Evas_Event_Key_Up*)event_info;
-	Evas_Object* choicebox = evas_object_name_find(e, "choicebox");
-
-	if(!strcmp(ev->keyname, "Escape")) {		
-		do_nothing();
-		return;
-	}
-	if(!strcmp(ev->keyname, "XF86Search") || !strcmp(ev->keyname, "x")) {
-		load_usbnet();
-		return;
-	}
-
-	choicebox_aux_key_down_handler(choicebox, ev);
+	Evas_Object* edje = evas_object_name_find(canvas, "edje");
+	evas_object_resize(edje, w, h);
 }
 
 int main(int argc, char **argv)
@@ -212,12 +172,6 @@ int main(int argc, char **argv)
 	setlocale(LC_ALL, "");
 	textdomain("usbwatcher");
 
-	usb_actions = (usb_action_t*)calloc(2, sizeof(usb_action_t));
-	asprintf(&usb_actions[0].name, "%s", gettext("Usb Mass Storage"));
-	usb_actions[0].handler = load_massstorage;
-	asprintf(&usb_actions[1].name, "%s", gettext("Charging"));
-	usb_actions[1].handler = do_nothing;
-
 	ecore_x_io_error_handler_set(x_shutdown, NULL);
 
 	ecore_con_server_add(ECORE_CON_LOCAL_USER, "usbwatcher", 0, NULL);
@@ -227,34 +181,32 @@ int main(int argc, char **argv)
 	ecore_event_handler_add(ECORE_CON_EVENT_CLIENT_DEL, _client_del, NULL);
 
 	main_win = ecore_evas_software_x11_new(0, 0, 0, 0, 600, 800);
-
 	ecore_evas_borderless_set(main_win, 0);
 	ecore_evas_shaped_set(main_win, 0);
 	ecore_evas_title_set(main_win, "Usbwatcher");
 	Evas *main_canvas = ecore_evas_get(main_win);
 
 	Evas_Object *main_edje = edje_object_add(main_canvas);
-	evas_object_name_set(main_edje, "main_edje");
-	edje_object_file_set(main_edje, DATADIR "/usbwatcher/usbwatcher.edj", "main_window");
-	edje_object_part_text_set(main_edje, "title", gettext("Usb connection"));
+	evas_object_name_set(main_edje, "edje");
+	edje_object_file_set(main_edje, DATADIR "/usbwatcher/usbwatcher.edj", "usbwatcher");
 	evas_object_move(main_edje, 0, 0);
 	evas_object_resize(main_edje, 600, 800);
 	evas_object_show(main_edje);
 
-	Evas_Object *choicebox = choicebox_new(main_canvas, "/usr/share/echoicebox/echoicebox.edj",
-			"full", item_handler,
-			draw_handler, page_handler, (void *)usb_actions);
-	choicebox_set_size(choicebox, 2); //sizeof(usb_actions) / sizeof(usb_actions[0]));
-	evas_object_name_set(choicebox, "choicebox");
-	edje_object_part_swallow(main_edje, "contents", choicebox);
-	evas_object_show(choicebox);
-
 	evas_object_focus_set(main_edje, true);
-	evas_object_event_callback_add(main_edje, EVAS_CALLBACK_KEY_UP, &key_up, NULL);
+	evas_object_event_callback_add(main_edje, EVAS_CALLBACK_KEY_UP, &key_handler, NULL);
 
 	ecore_evas_callback_resize_set(main_win, main_win_resize_handler);
-	//	ecore_evas_show(main_win);
 
+	edje_object_part_text_set(main_edje, "usbwatcher/title", gettext("USB Connection"));
+	char *t;
+	asprintf(&t, "%s<br><br>%s",
+			gettext("Usb Mass Storage - press \"1\""),
+			gettext("Battery Charging - press \"2\""));
+	edje_object_part_text_set(main_edje, "usbwatcher/text", t);
+	free(t);
+	
+//	ecore_evas_show(main_win);
 	ecore_main_loop_begin();
 
 	ecore_evas_free(main_win);
@@ -264,10 +216,6 @@ int main(int argc, char **argv)
 	ecore_con_shutdown();
 	ecore_shutdown();
 	evas_shutdown();
-
-	free(usb_actions[0].name);
-	free(usb_actions[1].name);
-	free(usb_actions);
 
 	return 0;
 }
